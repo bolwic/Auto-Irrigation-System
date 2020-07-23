01973bf51e7cdddebefb9ede86159563321da46f
bolwic <bolwic@gmail.com>
2020-07-23 12:25:03 -0700

    Update combinedtest.py

--- combinedtest.py
+++ combinedtest.py
@@ -3,6 +3,9 @@ import time
 import Adafruit_ADS1x15
 import math
 
+#this program is the first test to see if I can get the moisture senor and the relays to work together.
+#in this case, I also installed a sun lamp to the relay as I plan on placing the system indoors.
+
 channel1 = 7 #sun lamp
 channel2 = 13 #water pump
 
@@ -48,7 +51,7 @@ if __name__ == '__main__':
                 except:
                     print("OSError")
             print(max(value))
-            if(max(value) < 15300):
+            if(max(value) < 15300):#through testing, if the sensor reads highter than 15300, it means the soil is wet.
                 print("wet")
                 motor_off()
             else:
